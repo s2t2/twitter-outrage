7436fae7ea8a1ad3524cb5a22d2c5503d8dd95b7
Mike Rossetti <s2t2@users.noreply.github.com>
2019-12-15 20:02:06 -0500

    Cors error

--- src/client/src/utils/http.ts
+++ src/client/src/utils/http.ts
@@ -1,6 +1,5 @@
 
-//const API_BASE_URL = process.env.API_BASE_URL || "OOPS" // "http://localhost:3001"
-const API_BASE_URL = process.env.API_BASE_URL || "https://twitter-outrage-classification.herokuapp.com" // "http://localhost:3001"
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3001" // "https://twitter-outrage-classification.herokuapp.com"
 console.log("API BASE URL", API_BASE_URL)
 
 const get = async (entity: string): Promise<any[]> => {
